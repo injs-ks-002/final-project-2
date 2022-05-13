a8c27656d397485a513050c64b69b21515736047
marufr1 <marufrahman923@gmail.com>
2022-05-13 22:37:43 +0700

    Initial app production

--- config/config.js
+++ config/config.js
@@ -19,6 +19,13 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
+    "dialectOptions": {
+      "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+      }
+    }
   }
 }

cd946d7838e93173f1b9022a66d857c5b5714493
marufr1 <marufrahman923@gmail.com>
2022-06-05 21:21:20 +0700

    Add unit test for social media

--- routes/socialmedia.route.js
+++ routes/socialmedia.route.js
@@ -6,7 +6,7 @@ const socialmedia = require('../middleware/socialmedia.validation')
 
 router.post('/', auth.verify, socialmedia.validation, controller.postSocialMedia)
 router.get('/', auth.verify, controller.getAllSocialMedia)
-router.put('/:socialMediaId', auth.verify, controller.putSocialMedia)
+router.put('/:socialMediaId', auth.verify, socialmedia.validation, controller.putSocialMedia)
 router.delete('/:socialMediaId', auth.verify, controller.deleteSocialMedia)
 
 module.exports = router
